6976987e9d9e8fc351ac09a7286889236ef542ea
AkshitJain130 <akshit.jain.it.2022@miet.ac.in>
2024-06-13 18:26:28 +0530

    Add files via upload

---
+++ Sorry card Animation1.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    document.querySelector('.apology').style.display = 'block';
+  }, 2000); // 2000 milliseconds (2 seconds) delay
+});
